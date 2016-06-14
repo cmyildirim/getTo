19a260d3f6c08121d158859e024d1242c30ce73a
yosooff <yosooff@yosooff-MacBook-Pro.local>
2016-06-15 01:55:12 +0300

    androidworkfix

--- android/app/src/main/java/com/rntest/MainActivity.java
+++ android/app/src/main/java/com/rntest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.getto;
+package com.rntest;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "getto";
+        return "rntest";
     }
 
     /**
